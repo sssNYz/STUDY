63f2405a428662804b4f3275a97bc7a495b705a0
THUMATHAN SOPABOON <156821601+sssNYz@users.noreply.github.com>
2024-02-12 23:16:32 +0700

    Update LED_BUILTIN_BLINK.ino

--- BASIC ARDUINO/LAB1/1.1/LED_BUILTIN_BLINK.ino
+++ BASIC ARDUINO/LAB1/1.1/LED_BUILTIN_BLINK.ino
@@ -3,14 +3,17 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("on");
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(LED, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("off");
   delay(1000); // Wait for 1000 millisecond(s)
 }
